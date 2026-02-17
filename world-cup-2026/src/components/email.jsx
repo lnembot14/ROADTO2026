1d44ce9cf82fe185bca3cc9b5b247eda2dfc19b4
lnembot14 <laalinembot@gmail.com>
2026-02-17 13:32:13 -0500

    Created new email.jsx and names.jsx files

---
+++ world-cup-2026/src/components/email.jsx
@@ -0,0 +1,20 @@
+
+function Email() {
+  return (
+    <>
+      <div id="rsvp">
+        <h2 id="rsvp">RSVP List</h2>
+        <div class="rsvp-container">
+        <p>Do you want to be the first to know about everyhting #WORLDCUP2026? If so, you need to join our legacy email list. Once signed up you'll get access to exclusives like reduced ticket prices, meal plans and tourist attractions!</p>
+        <div class="rsvp-participants">
+            <p>🎟️ Doug Terrier has cemented their Legacy.</p>
+            <p>🎟️ Amethest Cancha has cemented their Legacy.</p>
+            <p>🎟️ Tobin Montreal has cemented their Legacy</p>
+        </div>
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default Email
