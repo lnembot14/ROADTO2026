1ad50842e61ddd672096660498ff578a173ae210
lnembot14 <laalinembot@gmail.com>
2026-02-17 13:08:51 -0500

    Added countdown.jsx for countdown section file

---
+++ world-cup-2026/src/components/countdown.jsx
@@ -0,0 +1,16 @@
+function Countdown() {
+  return (
+    <>
+        <div id="Schedule">
+                <div className = "countdown">
+                    <h2>Countdown to the #FIFA2026 World Cup</h2>
+            <p className="paragraph">A live countdown from any where, at any time, in any place to remind you how close we
+                are to the 2026 World Cup!
+            </p>
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default Countdown
