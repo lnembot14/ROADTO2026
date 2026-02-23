9d34fb7e964c469e944cc70a5ddd795b39636ca0
lnembot14 <laalinembot@gmail.com>
2026-02-23 12:50:29 -0500

    Added countdown effect using useState and useEffect

--- world-cup-2026/src/App.jsx
+++ world-cup-2026/src/App.jsx
@@ -4,20 +4,19 @@ import Navbar from './components/navbar'
 import Countdown from './components/countdown'
 import Email from './components/email'
 import Names from './components/names'
+import Gallery from './components/gallery'
 
 function App() {
   return (
-    <>
-      <Header />
-      <Navbar />
-      <Countdown />
-      <div className = "photo-gallery">
-          <h2> FIFA World Cup: Through Picture</h2>
-        </div>
-      <Email />
-      <Names />
-    </>
-  )
+      <div className="App"> 
+        <Header />
+        <Navbar />
+        <Countdown targetMS={new Date("2026-06-11T00:00:00").getTime()}/>
+        <Gallery />
+        <Email />
+        <Names />
+      </div>
+  );
 }
 
 export default App
--- world-cup-2026/src/components/countdown.jsx
+++ world-cup-2026/src/components/countdown.jsx
@@ -1,10 +1,36 @@
-function Countdown() {
+import { useEffect, useState } from "react"
+
+const Countdown = ({targetMS}) => {
+  const getTimeLeft = () => Math.max(targetMS - Date.now(), 0);
+  const[time, setTime] = useState(getTimeLeft)
+
+  useEffect(() =>{
+    setInterval(()=>{
+      setTime(getTimeLeft())
+    }, 1000)
+  }, [targetMS])
+
+  const getFormattedTime = (Milloseconds) =>{
+    let total_seconds = parseInt(Math.floor(Milloseconds/1000))
+    let total_minutes = parseInt(Math.floor(total_seconds/60))
+    let total_hours = parseInt(Math.floor(total_minutes/60))
+    let days = parseInt(Math.floor(total_hours/24))
+
+    let seconds = Math.floor(total_seconds%60)
+    let minutes = Math.floor(total_minutes%60)
+    let hours = Math.floor(total_hours%24)
+
+
+    return `${days} Days: ${hours} Hours: ${minutes} Minutes: ${seconds} seconds`
+
+  }
+
   return (
     <>
         <div id="Schedule">
                 <div className = "countdown">
                     <h2>Countdown to the #FIFA2026 World Cup</h2>
-                    <p>Clock: 20:00:00 </p>
+                    <p>{getFormattedTime(time)}</p>
             <p className="paragraph">A live countdown from any where, at any time, in any place to remind you how close we
                 are to the 2026 World Cup!
             </p>
