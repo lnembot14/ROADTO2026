c84c14bf1b753981b5ac1c806e338d7ef0177d43
Laali <laalinembot@gmail.com>
2026-04-01 18:34:21 -0400

    NEw changes

--- world-cup-2026/src/components/gallery.jsx
+++ world-cup-2026/src/components/gallery.jsx
@@ -1,3 +1,9 @@
+import photo1 from 'src/assets/images/img1.jpg'
+import photo2 from 'src/assets//images/img2.jpg'
+import photo3 from 'src/assets/images/img3.jpg'
+
+const pictures = [photo1, photo2, photo3]
+
 function Gallery() {
   return (
     <>
