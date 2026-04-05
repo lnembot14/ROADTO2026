7f8cfb51abb08bbd099a856cb513edab7cc93fc2
Laali <laalinembot@gmail.com>
2026-04-05 18:36:38 -0400

    Added email to the bottom of the list and turned into column

--- world-cup-2026/src/components/email.jsx
+++ world-cup-2026/src/components/email.jsx
@@ -32,13 +32,6 @@ function Email() {
         <p>Do you want to be the first to know about everyhting #WORLDCUP2026? If so, you need to join our legacy email list. Once signed up you'll get access to exclusives like reduced ticket prices, meal plans and tourist attractions!</p>
         <form onSubmit={handleSubmit}>
           <input
-            type="email"
-            placeholder="Enter your email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-          <input
             type="text"
             placeholder="Enter your first name"
             value={firstName}
@@ -50,6 +43,13 @@ function Email() {
             placeholder="Enter your last name"
             value={lastName}
             onChange={(e) => setLastName(e.target.value)}
+            required
+          />
+          <input
+            type="email"
+            placeholder="Enter your email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
             required
           />
           <button type="submit">Join List</button>
