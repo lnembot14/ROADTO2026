f5b16155c6952387a6f3b95560b58529de4adf39
Laali <laalinembot@gmail.com>
2026-04-05 19:25:50 -0400

    Added router to app.jsx page and created players to watch page

---
+++ world-cup-2026/src/components/PlayersToWatch.jsx
@@ -0,0 +1,10 @@
+
+
+function playersToWatch() {
+    return (
+        <div>
+            <h2>Players to Watch</h2>
+            <p>As the world gears up for the 2026 FIFA World Cup, fans and analysts alike are buzzing with excitement about the players to watch in this highly anticipated tournament. With a blend of seasoned veterans and emerging talents, the 2026 World Cup promises to showcase some of the best footballing skills on the global stage.</p>
+        </div>
+    )
+}
