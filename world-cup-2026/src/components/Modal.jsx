270f0ae36862e7d12953f4cc492360543c401c72
Laali <laalinembot@gmail.com>
2026-05-20 14:08:04 -0400

    Added individual modal for each player

--- world-cup-2026/src/components/Modal.jsx
+++ world-cup-2026/src/components/Modal.jsx
@@ -1,20 +1,15 @@
 import "./Modal.css";
 
-function Modal({ isOpen, onClose }) {
+function Modal({ isOpen, player, onClose }) {
   if (!isOpen) return null;
 
   return (
-    <div className="modal">
-      <div className="overlay"></div>
-      <div className="modal-content">
-        <button onClick={onClose}>X</button>
-        <h2>Hello World</h2>
-        <p>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Natus
-          laboriosam nobis nulla voluptates similique a sapiente odio quod
-          dolores dignissimos animi, perferendis totam mollitia tempora?
-          Nihil molestiae necessitatibus neque possimus!
-        </p>
+    <div className="player-modal">
+      <div className="player-modal-overlay"></div>
+      <div className="player-modal-content">
+        <button className= "player-modal-close-button" onClick={onClose}>X</button>
+        <h2> {player.player} </h2>
+        <p> {player.bio} </p>
       </div>
     </div>
   );
--- world-cup-2026/src/components/PlayersToWatch.jsx
+++ world-cup-2026/src/components/PlayersToWatch.jsx
@@ -12,7 +12,7 @@ const vincius_image = "/images/vini-img.jpg"
 
 const players = [
     {
-        player: "mbappe",
+        player: "Kylian Mbappe",
         country: "France",
         team : "Real Madrid",
         img : mbappe_image,
@@ -21,7 +21,7 @@ const players = [
     },
 
     {
-        player: "haaland",
+        player: "Erling Haaland",
         country: "Norway",
         team : "Manchester City",
         img : haaland_image,
@@ -30,7 +30,7 @@ const players = [
     },
 
     {
-        player: "yamal",
+        player: "Lamine Yamal",
         country: "Spain",
         team : "Barcelona",
         img : yamal_image,
@@ -39,7 +39,7 @@ const players = [
     },
 
     {
-        player: "Messi",
+        player: "Lionel Messi",
         country: "Argentina",
         team : "Inter Miami",
         img : messi_image,
@@ -48,7 +48,7 @@ const players = [
     },
 
     {
-        player: "Kane",
+        player: "Harry Kane",
         country: "England",
         team : "Bayern Munich",
         img : kane_image,
@@ -57,7 +57,7 @@ const players = [
     },
 
     {
-        player: "Vinicius",
+        player: "Vinicius Junior",
         country: "Brazil",
         team : "Real Madrid",
         img : vincius_image,
@@ -69,53 +69,42 @@ const players = [
 
 
 function PlayersToWatch() {
-    const [selectedPlayer, setSelectedPlayer] = useState(0)
-    
-    function handleClick(){
-        setSelectedPlayer(selectedPlayer + 2)
-    }
 
-    const[playerStats, setPlayerStats] = useState(false)
-
-    function showPlayerStats(){
-        setPlayerStats(true)
-    }
+    const[selectedPlayer, setSelectedPlayer] = useState(null)
 
     return (
         <>
         <div className="intro">
             <h1>Players to Watch</h1>
             <p>As the world gears up for the 2026 FIFA World Cup, fans and analysts alike are buzzing with excitement about the players to watch in this highly anticipated tournament. With a blend of seasoned veterans and emerging talents, the 2026 World Cup promises to showcase some of the best footballing skills on the global stage.</p>
-            <button onClick={handleClick}>Update</button>
-            <p>Count: {selectedPlayer}</p>
         </div>
         <div className= "players-list">
-            <button onClick={showPlayerStats}>
+            <button className="player-button" onClick={()=>setSelectedPlayer(players[0])}>
                 <div className= "player-card" id="mbappe"> 
                 <img src = {mbappe_image} alt="Kylian Mbappe" className= "player-image" /> <p className = "player-name">Kylian Mbappe</p> </div>
             </button>
-            <button>
-                <div className= "player-card" id="haaland"> \
+            <button className="player-button" onClick={()=>setSelectedPlayer(players[1])}>
+                <div className= "player-card" id="haaland">
                 <img src = {haaland_image} alt="Erling Haaland" className= "player-image" /> <p className = "player-name">Erling Haaland</p> </div>
             </button>
-            <button>
+            <button className="player-button" onClick = {()=>setSelectedPlayer(players[2])}>
                 <div className= "player-card" id="yamal"> 
                 <img src = {yamal_image} alt="Lamine Yamal" className= "player-image" /> <p className = "player-name">Lamine Yamal</p></div>
             </button>
-            <button>
+            <button className="player-button" onClick = {()=>setSelectedPlayer(players[3])}>
                 <div className= "player-card" id="messi"> 
                 <img src = {messi_image} alt="Lionel Messi" className= "player-image" /> <p className = "player-name">Lionel Messi</p> </div>
             </button>
-            <button>
+            <button className="player-button" onClick={()=> setSelectedPlayer(players[4])}>
                 <div className= "player-card" id="kane"> 
                 <img src = {kane_image} alt="Harry Kane" className= "player-image" /> <p className = "player-name">Harry Kane</p> </div>
             </button>
-            <button>
+            <button className="player-button" onClick={()=> setSelectedPlayer(players[5])}>
                 <div className= "player-card" id="vincius"> 
                 <img src = {vincius_image} alt="Vincius Junior" className= "player-image" /> <p className = "player-name">Vincius Junior</p> </div>
             </button>
         </div>
-        <Modal isOpen = {playerStats} />
+        <Modal isOpen = {selectedPlayer !== null}  player={selectedPlayer} onClose= {()=> setSelectedPlayer(null)}/>
         </>
     )
 }
