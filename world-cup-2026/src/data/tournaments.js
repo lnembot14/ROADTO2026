6f1ab199189336fb1be7710054be0612cd2bb7eb
Laali <laalinembot@gmail.com>
2026-05-24 18:22:27 -0400

    Added world cup facts in array (up to 2006)

---
+++ world-cup-2026/src/data/tournaments.js
@@ -0,0 +1,56 @@
+const tournaments = [
+    {
+        year: 2022, 
+        host: "Qatar", 
+        champion: "Argentina", 
+        runnerUp: "France",
+        topScorer: "Kylian Mbappé", 
+        goldenBall: "Lionel Messi", 
+        finalScore: "3-3 (AET) 4-2 pens",
+        funFact: "First World Cup held in the Middle East..."
+    },
+
+    {
+        year: 2018,
+        host: "Russia", 
+        champion: "France",
+        runnerUp: "Croatia",
+        topScorer: "Harry Kane",
+        goldenBall: "Luka Modric",
+        finalScore: "4-2",
+        funFact: "None of the top five most populous countries on Earth qualified for the 2018 FIFA World Cup in Russia..."
+    },
+
+    {
+        year: 2014,
+        host: "Brazil",
+        champion: "Germany",
+        runnerUp: "Argentina",
+        topScorer: "James Rodriguez",
+        goldenBall: "Lionel Messi",
+        finalScore: "1-0",
+        funFact: "Mario Götze's tournament-winning goal made him the very first substitute in history to score the winning goal in a World Cup final..."
+    },
+
+    {
+        year: 2010,
+        host: "South Africa",
+        champion: "Spain",
+        runnerUp: "Netherlands",
+        topScorer: "Thomas Muller",
+        goldenBall: "Diego Forlan",
+        finalScore: "1-0",
+        funFact: "Spain became the first European nation to win the FIFA World Cup outside of their home continent"
+    },
+
+    {
+        year: 2006,
+        host: "Germany",
+        champion: "Italy",
+        runnerUp: "France",
+        topScorer: "Miroslav Klose",
+        goldenBall: "Zinedine Zidane",
+        finalScore: "1-1 (AET) 5-3 pens",
+        funFact: "It featured a chaotic Battle of Nuremberg with four red cards, an English referee giving a player three yellow cards before sending him off",
+    }
+]
