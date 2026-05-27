7bbfdece6703a11f30ad2e55bcd756da88545d31
Laali <laalinembot@gmail.com>
2026-05-27 19:51:48 -0400

    Add navbar and update tournament data

--- world-cup-2026/src/App.jsx
+++ world-cup-2026/src/App.jsx
@@ -3,18 +3,21 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import PlayersToWatch from './components/PlayersToWatch'
 import Stadiums from './components/Stadiums'
 import HomePage from './components/HomePage'
-import Timeline from './components/timeline';
 import Cities from './components/Cities'
+import HistoryPage from "./components/HistoryPage"
+import Navbar from './components/navbar'
 
 function App() {
   return (
+
     <BrowserRouter>
+    <Navbar />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/PlayersToWatch" element={<PlayersToWatch />} />
         <Route path="/Cities" element={< Cities />} />
         <Route path="/Stadiums" element={< Stadiums />} />
-        <Route path="/Timeline" element={< Timeline />} />
+        <Route path="/HistoryPage" element={< HistoryPage />} />
       </Routes>
     </BrowserRouter>
   );
--- world-cup-2026/src/data/tournaments.js
+++ world-cup-2026/src/data/tournaments.js
@@ -55,189 +55,189 @@ export const tournaments = [
     },
 
     {
-    year: 2002,
-    host: "South Korea/Japan",
-    champion: "Brazil",
-    runnerUp: "Germany",
-    topScorer: "Ronaldo",
-    goldenBall: "Oliver Kahn",
-    finalScore: "2-0",
-    funFact: "This was the first World Cup hosted in Asia and the first hosted by two countries."
-},
+        year: 2002,
+        host: "South Korea/Japan ",
+        champion: "Brazil",
+        runnerUp: "Germany",
+        topScorer: "Ronaldo",
+        goldenBall: "Oliver Kahn",
+        finalScore: "2-0",
+        funFact: "This was the first World Cup hosted in Asia and the first hosted by two countries."
+    },
 
-{
-    year: 1998,
-    host: "France",
-    champion: "France",
-    runnerUp: "Brazil",
-    topScorer: "Davor Šuker",
-    goldenBall: "Ronaldo",
-    finalScore: "3-0",
-    funFact: "France won their first ever World Cup title on home soil."
-},
+    {
+        year: 1998,
+        host: "France ",
+        champion: "France",
+        runnerUp: "Brazil",
+        topScorer: "Davor Šuker",
+        goldenBall: "Ronaldo",
+        finalScore: "3-0",
+        funFact: "France won their first ever World Cup title on home soil."
+    },
 
-{
-    year: 1994,
-    host: "United States",
-    champion: "Brazil",
-    runnerUp: "Italy",
-    topScorer: "Oleg Salenko and Hristo Stoichkov",
-    goldenBall: "Romário",
-    finalScore: "0-0 (AET) 3-2 pens",
-    funFact: "This was the first World Cup final to be decided by a penalty shootout."
-},
+    {
+        year: 1994,
+        host: "United States ",
+        champion: "Brazil",
+        runnerUp: "Italy",
+        topScorer: "Oleg Salenko and Hristo Stoichkov",
+        goldenBall: "Romário",
+        finalScore: "0-0 (AET) 3-2 pens",
+        funFact: "This was the first World Cup final to be decided by a penalty shootout."
+    },
 
-{
-    year: 1990,
-    host: "Italy",
-    champion: "West Germany",
-    runnerUp: "Argentina",
-    topScorer: "Salvatore Schillaci",
-    goldenBall: "Salvatore Schillaci",
-    finalScore: "1-0",
-    funFact: "West Germany won their third World Cup before German reunification later that year."
-},
+    {
+        year: 1990,
+        host: "Italy ",
+        champion: "West Germany",
+        runnerUp: "Argentina",
+        topScorer: "Salvatore Schillaci",
+        goldenBall: "Salvatore Schillaci",
+        finalScore: "1-0",
+        funFact: "West Germany won their third World Cup before German reunification later that year."
+    },
 
-{
-    year: 1986,
-    host: "Mexico",
-    champion: "Argentina",
-    runnerUp: "West Germany",
-    topScorer: "Gary Lineker",
-    goldenBall: "Diego Maradona",
-    finalScore: "3-2",
-    funFact: "Diego Maradona scored both the 'Hand of God' goal and the 'Goal of the Century' in this tournament."
-},
+    {
+        year: 1986,
+        host: "Mexico ",
+        champion: "Argentina",
+        runnerUp: "West Germany",
+        topScorer: "Gary Lineker",
+        goldenBall: "Diego Maradona",
+        finalScore: "3-2",
+        funFact: "Diego Maradona scored both the 'Hand of God' goal and the 'Goal of the Century' in this tournament."
+    },
 
-{
-    year: 1982,
-    host: "Spain",
-    champion: "Italy",
-    runnerUp: "West Germany",
-    topScorer: "Paolo Rossi",
-    goldenBall: "Paolo Rossi",
-    finalScore: "3-1",
-    funFact: "Paolo Rossi finished as top scorer and helped lead Italy to their third World Cup title."
-},
+    {
+        year: 1982,
+        host: "Spain ",
+        champion: "Italy",
+        runnerUp: "West Germany",
+        topScorer: "Paolo Rossi",
+        goldenBall: "Paolo Rossi",
+        finalScore: "3-1",
+        funFact: "Paolo Rossi finished as top scorer and helped lead Italy to their third World Cup title."
+    },
 
-{
-    year: 1978,
-    host: "Argentina",
-    champion: "Argentina",
-    runnerUp: "Netherlands",
-    topScorer: "Mario Kempes",
-    goldenBall: "N/A",
-    finalScore: "3-1 (AET)",
-    funFact: "Argentina won their first World Cup title while hosting the tournament."
-},
+    {
+        year: 1978,
+        host: "Argentina ",
+        champion: "Argentina",
+        runnerUp: "Netherlands",
+        topScorer: "Mario Kempes",
+        goldenBall: "N/A",
+        finalScore: "3-1 (AET)",
+        funFact: "Argentina won their first World Cup title while hosting the tournament."
+    },
 
-{
-    year: 1974,
-    host: "West Germany",
-    champion: "West Germany",
-    runnerUp: "Netherlands",
-    topScorer: "Grzegorz Lato",
-    goldenBall: "N/A",
-    finalScore: "2-1",
-    funFact: "This was the first World Cup to use the current FIFA World Cup trophy."
-},
+    {
+        year: 1974,
+        host: "West Germany ",
+        champion: "West Germany",
+        runnerUp: "Netherlands",
+        topScorer: "Grzegorz Lato",
+        goldenBall: "N/A",
+        finalScore: "2-1",
+        funFact: "This was the first World Cup to use the current FIFA World Cup trophy."
+    },
 
-{
-    year: 1970,
-    host: "Mexico",
-    champion: "Brazil",
-    runnerUp: "Italy",
-    topScorer: "Gerd Müller",
-    goldenBall: "N/A",
-    finalScore: "4-1",
-    funFact: "Brazil became the first country to win the World Cup three times."
-},
+    {
+        year: 1970,
+        host: "Mexico ",
+        champion: "Brazil",
+        runnerUp: "Italy",
+        topScorer: "Gerd Müller",
+        goldenBall: "N/A",
+        finalScore: "4-1",
+        funFact: "Brazil became the first country to win the World Cup three times."
+    },
 
-{
-    year: 1966,
-    host: "England",
-    champion: "England",
-    runnerUp: "West Germany",
-    topScorer: "Eusébio",
-    goldenBall: "N/A",
-    finalScore: "4-2 (AET)",
-    funFact: "England won their first and only World Cup title at Wembley Stadium."
-},
+    {
+        year: 1966,
+        host: "England ",
+        champion: "England",
+        runnerUp: "West Germany",
+        topScorer: "Eusébio",
+        goldenBall: "N/A",
+        finalScore: "4-2 (AET)",
+        funFact: "England won their first and only World Cup title at Wembley Stadium."
+    },
 
-{
-    year: 1962,
-    host: "Chile",
-    champion: "Brazil",
-    runnerUp: "Czechoslovakia",
-    topScorer: "Garrincha, Vavá, Leonel Sánchez, Flórián Albert, Valentin Ivanov, and Dražan Jerković",
-    goldenBall: "N/A",
-    finalScore: "3-1",
-    funFact: "Brazil defended their 1958 title even though Pelé was injured early in the tournament."
-},
+    {
+        year: 1962,
+        host: "Chile ",
+        champion: "Brazil",
+        runnerUp: "Czechoslovakia",
+        topScorer: "Garrincha, Vavá, Leonel Sánchez, Flórián Albert, Valentin Ivanov, and Dražan Jerković",
+        goldenBall: "N/A",
+        finalScore: "3-1",
+        funFact: "Brazil defended their 1958 title even though Pelé was injured early in the tournament."
+    },
 
-{
-    year: 1958,
-    host: "Sweden",
-    champion: "Brazil",
-    runnerUp: "Sweden",
-    topScorer: "Just Fontaine",
-    goldenBall: "N/A",
-    finalScore: "5-2",
-    funFact: "A 17-year-old Pelé became a global star and scored twice in the final."
-},
+    {
+        year: 1958,
+        host: "Sweden ",
+        champion: "Brazil",
+        runnerUp: "Sweden",
+        topScorer: "Just Fontaine",
+        goldenBall: "N/A",
+        finalScore: "5-2",
+        funFact: "A 17-year-old Pelé became a global star and scored twice in the final."
+    },
 
-{
-    year: 1954,
-    host: "Switzerland",
-    champion: "West Germany",
-    runnerUp: "Hungary",
-    topScorer: "Sándor Kocsis",
-    goldenBall: "N/A",
-    finalScore: "3-2",
-    funFact: "West Germany's comeback win over Hungary became known as the 'Miracle of Bern.'"
-},
+    {
+        year: 1954,
+        host: "Switzerland ",
+        champion: "West Germany",
+        runnerUp: "Hungary",
+        topScorer: "Sándor Kocsis",
+        goldenBall: "N/A",
+        finalScore: "3-2",
+        funFact: "West Germany's comeback win over Hungary became known as the 'Miracle of Bern.'"
+    },
 
-{
-    year: 1950,
-    host: "Brazil",
-    champion: "Uruguay",
-    runnerUp: "Brazil",
-    topScorer: "Ademir",
-    goldenBall: "N/A",
-    finalScore: "2-1",
-    funFact: "There was no official final; Uruguay beat Brazil in the decisive final-round match known as the Maracanazo."
-},
+    {
+        year: 1950,
+        host: "Brazil ",
+        champion: "Uruguay",
+        runnerUp: "Brazil",
+        topScorer: "Ademir",
+        goldenBall: "N/A",
+        finalScore: "2-1",
+        funFact: "There was no official final; Uruguay beat Brazil in the decisive final-round match known as the Maracanazo."
+    },
 
-{
-    year: 1938,
-    host: "France",
-    champion: "Italy",
-    runnerUp: "Hungary",
-    topScorer: "Leônidas",
-    goldenBall: "N/A",
-    finalScore: "4-2",
-    funFact: "Italy became the first nation to successfully defend the World Cup title."
-},
+    {
+        year: 1938,
+        host: "France ",
+        champion: "Italy",
+        runnerUp: "Hungary",
+        topScorer: "Leônidas",
+        goldenBall: "N/A",
+        finalScore: "4-2",
+        funFact: "Italy became the first nation to successfully defend the World Cup title."
+    },
 
-{
-    year: 1934,
-    host: "Italy",
-    champion: "Italy",
-    runnerUp: "Czechoslovakia",
-    topScorer: "Oldřich Nejedlý",
-    goldenBall: "N/A",
-    finalScore: "2-1 (AET)",
-    funFact: "This was the first World Cup to include a qualification stage."
-},
+    {
+        year: 1934,
+        host: "Italy ",
+        champion: "Italy",
+        runnerUp: "Czechoslovakia",
+        topScorer: "Oldřich Nejedlý",
+        goldenBall: "N/A",
+        finalScore: "2-1 (AET)",
+        funFact: "This was the first World Cup to include a qualification stage."
+    },
 
-{
-    year: 1930,
-    host: "Uruguay",
-    champion: "Uruguay",
-    runnerUp: "Argentina",
-    topScorer: "Guillermo Stábile",
-    goldenBall: "N/A",
-    finalScore: "4-2",
-    funFact: "This was the first ever FIFA World Cup, and Uruguay won it on home soil."
-}
+    {
+        year: 1930,
+        host: "Uruguay ",
+        champion: "Uruguay",
+        runnerUp: "Argentina",
+        topScorer: "Guillermo Stábile",
+        goldenBall: "N/A",
+        finalScore: "4-2",
+        funFact: "This was the first ever FIFA World Cup, and Uruguay won it on home soil."
+    }
 ]
