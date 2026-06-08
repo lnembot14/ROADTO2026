4f88f8ccbe073908ef41694956d317d1cb719d84
Laali <laalinembot@gmail.com>
2026-06-08 17:59:39 -0400

    Added props and attributes to Tournament Card

--- world-cup-2026/src/components/TournamentCard.jsx
+++ world-cup-2026/src/components/TournamentCard.jsx
@@ -1,7 +1,12 @@
-function TournamentCard(){
+function TournamentCard({year, flag, host, champions}){
     return (
         <>
-        
+            <div className="tournaments-box">
+                <h2> {year} </h2>
+                <h2> {flag} </h2>
+                <h2> {host} </h2>
+                <h2> {champions} </h2>
+            </div>
         </>
     )
 }
